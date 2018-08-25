b688e3b3997214e7b2020299b59761b439d6f3ac
Ian Ishmael Oderon <ianoderon@gmail.com>
2018-08-25 12:44:12 +0800

    init

---
+++ src/polygon/PolygonFactory.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polygon;
+
+/**
+ *
+ * @author ianoderon
+ */
+public class PolygonFactory {
+    public Polygon getPolygon(int numberOfSides) {
+        if(numberOfSides == 3) {
+            return new Triangle();
+        }
+        if(numberOfSides == 4){
+            return new Square();
+        } 
+        if(numberOfSides == 5){
+            return new Pentagon();
+        }
+    }
+}
